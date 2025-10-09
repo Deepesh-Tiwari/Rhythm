b05f84c5b7542569b8f4c70f8b461f0ed7073646
Deepesh Tiwari <deepeshtiwari@Deepeshs-MacBook-Air.local>
2025-10-09 06:35:14 +0530

    added test user routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,44 @@ const express = require('express');
 
 const app = express();
 
+
+// Response Handler
+app.get("/user",(req, res) => {
+    res.send("get call to user route");
+})
+
+// Response Handler
+app.put("/user",(req, res) => {
+    res.send("put call to user route");
+})
+
+// Response Handler
+app.patch("/user",(req, res) => {
+    res.send("patch call to user route");
+})
+
+// Response Handler
+app.post("/user",(req, res) => {
+    res.send("post call to user route");
+})
+
+// Response Handler
+app.delete("/user",(req, res) => {
+    res.send("delete call to user route");
+})
+
+
+// Response Handler
+app.get("/test",(req, res) => {
+    res.send({
+        firstname: "Deepesh",
+        lastname :"Tiwari"
+    });
+})
+
 // Response Handler
 app.use("/test",(req, res) => {
     res.send("Hello from the server test page!!!");
-})
-
-app.use("/",(req, res) => {
-    res.send("Hello from the server dashboard!!!");
 })
 
 
